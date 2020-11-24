434a8c37adbdab684db62d9e226624ec1d4106ec
alliancecoder <sboyd@electricaltrainingalliance.org>
2020-11-23 21:59:37 -0500

    new project from quarkus-maven - ready for development

---
+++ src/test/java/org/protechskillsinstitute/learning/NativeqkafkaIT.java
@@ -0,0 +1,9 @@
+package org.protechskillsinstitute.learning;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeqkafkaIT extends qkafkaTest {
+
+    // Execute the same tests but in native mode.
+}
